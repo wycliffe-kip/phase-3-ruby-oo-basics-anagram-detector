45561269060109551d8e41ce4129c2604b0bb6f9
Wycliffe <wycliffekiprono4@gmail.com>
2023-05-18 20:39:28 +0300

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+    def initialize(word)
+        @word = word
+    end 
+
+    def match(anagrams)
+        anagrams.select { |anagram| anagram.downcase.chars.sort == @word.downcase.chars.sort }
+    end 
+end 
+
+listen = Anagram.new("listen")
+listen.match(%w[enlists google inlets banana])
